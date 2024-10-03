7ab3ffa2bb6f454741cc96d7890d5801d7167af8
ERO <zzhao260985652@gmail.com>
2024-10-03 13:59:56 -0400

    update set2Controller and step definitions

--- src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet2Controller.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet2Controller.java
@@ -1,28 +1,55 @@
 package ca.mcgill.ecse.coolsupplies.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CoolSuppliesFeatureSet2Controller {
 
+  private static List<TOStudent> students = new ArrayList<>();
+
   public static String addStudent(String name, String gradeLevel) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    for(TOStudent student: students){
+      if(student.getName().equals(name)){
+        return "Student "+ name + " has been already added to the list.";
+      }
+    }
+    students.add(new TOStudent(name,gradeLevel));
+    return "Student "+ name + " has been successfully added to the list.";
   }
 
   public static String updateStudent(String name, String newName, String newGradeLevel) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    for(TOStudent student: students){
+      if(student.getName().equals(name)){
+        students.remove(student);
+        students.add(new TOStudent(newName,newGradeLevel));
+        return "Student "+ name + " has been successfully removed from the list.";
+      }
+    }
+    return "Student "+name+" cannot be found in the list.";
   }
 
   public static String deleteStudent(String name) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    for(TOStudent student: students){
+      if(student.getName().equals(name)){
+        students.remove(student);
+        return "Student "+ name + " has been successfully removed from the list.";
+      }
+    }
+    return "Student "+name+" cannot be found in the list.";
   }
 
   public static TOStudent getStudent(String name) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    for(TOStudent student: students){
+      if(student.getName().equals(name)){
+        return student;
+      }
+    }
+    return null;
   }
 
   // returns all students
   public static List<TOStudent> getStudents() {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    return students;
   }
 
 }
