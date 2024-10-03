f20fa482563cc04557ae9986d23b96791c2b43db
ERO <zzhao260985652@gmail.com>
2024-10-03 18:36:01 -0400

    update step definitions

--- src/test/java/ca/mcgill/ecse/coolsupplies/features/test/GetParentStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/coolsupplies/features/test/GetParentStepDefinitions.java
@@ -13,19 +13,23 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class GetParentStepDefinitions {
+
+    List<TOParent> actualParent = new ArrayList<>();
     List<TOParent> data = new ArrayList<>();
-    List<TOParent> actualParents = new ArrayList<>();
 
     @Given("the following parent entities exists in the system \\(p8)")
     public void the_following_parent_entities_exists_in_the_system_p8(io.cucumber.datatable.DataTable dataTable) {
+
+        //remove all the instances from the database for each scenario
+        CoolSuppliesFeatureSet1Controller.getParents().clear();
+
         List<Map<String, String>> rows = dataTable.asMaps();
         for (var row : rows) {
             String email = row.get("email");
             String password = row.get("password");
             String name = row.get("name");
             int phoneNumber = Integer.parseInt(row.get("phoneNumber"));
-
-            data.add(new TOParent(email, password, name, phoneNumber));
+            CoolSuppliesFeatureSet1Controller.addParent(email,password,name,phoneNumber);
         }
     }
 
@@ -33,23 +37,19 @@ public class GetParentStepDefinitions {
     public void the_school_admin_attempts_to_get_from_the_system_the_parent_with_email_p8(
             String string) {
         // Write code here that turns the phrase above into concrete actions
-        for (TOParent parent : data) {
-            if (parent.getEmail().equals(string)) {
-                actualParents.add(parent);
-            }
-        }
+        actualParent.add(CoolSuppliesFeatureSet1Controller.getParent(string));
     }
 
     @When("the school admin attempts to get from the system all the parents \\(p8)")
     public void the_school_admin_attempts_to_get_from_the_system_all_the_parents_p8() {
         // Write code here that turns the phrase above into concrete actions
-        actualParents.addAll(data);
+        actualParent.addAll(CoolSuppliesFeatureSet1Controller.getParents());
     }
 
     @Then("the number of parent entities in the system shall be {string} \\(p8)")
     public void the_number_of_parent_entities_in_the_system_shall_be_p8(String string) {
         // Write code here that turns the phrase above into concrete actions
-        assertEquals(string, String.valueOf(data.size()));
+        assertEquals(string, String.valueOf(CoolSuppliesFeatureSet1Controller.getParents().size()));
     }
 
     @Then("the following parent entities shall be presented \\(p8)")
@@ -64,11 +64,12 @@ public class GetParentStepDefinitions {
         // For other transformations you can register a DataTableType.
 
         List<Map<String, String>> expectedParents = dataTable.asMaps();
-        assertEquals(expectedParents.size(), actualParents.size());
+
+        assertEquals(expectedParents.size(), actualParent.size());
 
         for (int i = 0; i < expectedParents.size(); i++) {
             Map<String, String> e = expectedParents.get(i);
-            TOParent a = actualParents.get(i);
+            TOParent a = actualParent.get(i);
             assertEquals(e.get("email"), a.getEmail());
             assertEquals(e.get("password"), a.getPassword());
             assertEquals(e.get("name"), a.getName());
@@ -79,9 +80,8 @@ public class GetParentStepDefinitions {
     @Then("no parent entities shall be presented \\(p8)")
     public void no_parent_entities_shall_be_presented_p8() {
         // Write code here that turns the phrase above into concrete actions
-        if (actualParents.isEmpty()) {
-            actualParents = null;  // Set to null if empty
+        if (actualParent.isEmpty()) {
+            assertNull(null);
         }
-        assertNull(actualParents);
     }
 }
