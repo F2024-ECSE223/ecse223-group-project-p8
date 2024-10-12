fefa70e944dbcaefe3c8fec925928169649b34ab
Mary Li <152451791+Uffy-oo@users.noreply.github.com>
2024-10-12 13:20:03 -0400

    DeleteParentStepDefinition + Controller1

--- src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet1Controller.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet1Controller.java
@@ -1,33 +1,78 @@
 package ca.mcgill.ecse.coolsupplies.controller;
 
 import java.util.List;
+import java.util.ArrayList;
+import ca.mcgill.ecse.coolsupplies.model.User;
+import ca.mcgill.ecse.coolsupplies.model.CoolSupplies;
+import ca.mcgill.ecse.coolsupplies.model.Parent;
+import ca.mcgill.ecse.coolsupplies.model.SchoolAdmin;
 
 public class CoolSuppliesFeatureSet1Controller {
 
+  private static CoolSupplies coolSupplies = new CoolSupplies();
+
   public static String updateAdmin(String password) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    if (coolSupplies.hasAdmin()){
+      SchoolAdmin schoolAdmin = coolSupplies.getAdmin();
+      schoolAdmin.setPassword(password);
+      return "Passord is updated successfully.";
+    }
+    else{
+      return "Admin Account doesn't exist";
+    }
   }
 
   public static String addParent(String email, String password, String name, int phoneNumber) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    //gai
+    Parent parent = new Parent(email, password, name, phoneNumber, coolSupplies);
+    coolSupplies.addParent(parent);
+    return "Parent is added successfully.";
   }
 
   public static String updateParent(String email, String newPassword, String newName,
       int newPhoneNumber) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+        //改
+    Parent parent = (Parent) User.getWithEmail(email);
+
+    if (parent != null){
+      parent.setName(newName);
+      parent.setPassword(newPassword);
+      parent.setPhoneNumber(newPhoneNumber);
+      return "Account is updated successfully.";
+    }
+    else{
+      return "Account doesn't exist.";
+    }
   }
 
   public static String deleteParent(String email) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    Parent parent = (Parent) User.getWithEmail(email);
+    if (parent != null){
+      parent.delete();
+      return "Account is deleted successfully.";
+    }
+    else{
+      return "The parent does not exist.";
+    }
   }
 
   public static TOParent getParent(String email) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    Parent parent = (Parent) User.getWithEmail(email);
+    TOParent toparent = new TOParent(parent.getEmail(), parent.getPassword(), parent.getName(), parent.getPhoneNumber());
+    return toparent;
   }
 
   // returns all parents
   public static List<TOParent> getParents() {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    List<Parent> parents = coolSupplies.getParents();
+    List<TOParent> toParents = new ArrayList<>();
+
+    for (Parent parent : parents) {
+      TOParent toParent = new TOParent(parent.getEmail(), parent.getPassword(), parent.getName(), parent.getPhoneNumber());
+      toParents.add(toParent);
+    }
+
+    return toParents;
   }
 
 }
