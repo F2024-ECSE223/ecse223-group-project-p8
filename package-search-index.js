9deb88d3c76e2ff6c17d27ae334fd8d3a7865e52
RENOLEVES <zzhao260985652@gmail.com>
2024-10-24 14:25:39 -0400

    update

--- package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ca.mcgill.ecse.coolsupplies.controller"}];updateSearchResults();
--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ca.mcgill.ecse.coolsupplies.controller","l":"CoolSuppliesFeatureSet2Controller"}];updateSearchResults();
