64cec93a136c0cfdc88c48f86780385c8f76a8d7
Mary Li <152451791+Uffy-oo@users.noreply.github.com>
2024-11-13 11:22:47 -0500

    Add Javafx Folder

---
+++ src/main/java/ca/mcgill/ecse/coolsupplies/javafx/fxml/CoolSuppliesFxmlView.java
@@ -0,0 +1,16 @@
+package ca.mcgill.ecse.coolsupplies.javafx.fxml;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CoolSuppliesFxmlView extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
