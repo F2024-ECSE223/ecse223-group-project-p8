c23168092c157cc7862ab527b1c34e9beb9b4b88
sen-snigdha <snigdha.dawson@gmail.com>
2024-11-18 01:15:05 -0500

    In-progress fxml
    In-progress fxml files for GUI page linking

---
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controllers/AddItemToOrderController.java
@@ -0,0 +1,22 @@
+package ca.mcgill.ecse.coolsupplies.controllers; /**
+ * Sample Skeleton for 'AddItemToOrder.fxml' Controller Class
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class AddItemToOrderController {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+
+    }
+
+}
