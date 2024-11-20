5e49e4b406a17e7c520ea6ad4b500d3cc65149ed
Shengyi Z <shengyi.zhong@mail.mcgill.ca>
2024-11-20 03:04:42 -0500

    update StudentsPageController

--- src/main/java/ca/mcgill/ecse/coolsupplies/javafx/fxml/controllers/StudentsPageController.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/javafx/fxml/controllers/StudentsPageController.java
@@ -1,13 +1,70 @@
 package ca.mcgill.ecse.coolsupplies.javafx.fxml.controllers;
 
+import ca.mcgill.ecse.coolsupplies.application.CoolSuppliesApplication;
+import ca.mcgill.ecse.coolsupplies.controller.*;
+import ca.mcgill.ecse.coolsupplies.model.CoolSupplies;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
-public class StudentsPageController {
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class StudentsPageController implements Initializable {
 
     @FXML
     private AnchorPane ap;
+    @FXML
+    private TextField studentTextField;
+    @FXML
+    private Button Add;
+    @FXML
+    private Label msgLabel;
+    @FXML
+    private ChoiceBox<TOParent> parentChoiceBox;
+
+    CoolSupplies coolSupplies = CoolSuppliesApplication.getCoolSupplies();
+    String studentName;
+    String parentEmail;
+    //    TOStudent selectedStudent;
+//    TOParent selectedParent;
+    List<TOParent> parents = CoolSuppliesFeatureSet1Controller.getParents();
+
+    @FXML
+    void addStudentToParent(ActionEvent event) {
+        try {
+            studentName = studentTextField.getText();
+//            selectedStudent = CoolSuppliesFeatureSet2Controller.getStudent(studentName);
+            String msg = CoolSuppliesFeatureSet6Controller.addStudentToParent(studentName, parentEmail);
+            msgLabel.setText(msg);
+//            System.out.println(studentName);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @FXML
+    void deleteStudentFromParent(ActionEvent event) {
+        try {
+            studentName = studentTextField.getText();
+            String msg = CoolSuppliesFeatureSet6Controller.deleteStudentFromParent(studentName, parentEmail);
+            msgLabel.setText(msg);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    void getParent(ActionEvent event) {
+        TOParent selectedParent = parentChoiceBox.getValue();
+        parentEmail = selectedParent.getEmail();
+    }
 
     @FXML
     void UpdateAccountPage(MouseEvent event) {
@@ -19,4 +76,8 @@ public class StudentsPageController {
 
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        parentChoiceBox.getItems().addAll(parents);
+    }
 }
