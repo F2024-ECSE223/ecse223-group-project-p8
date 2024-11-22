35313447aa937e908085d6b58cdd9ccefd08f2a3
Shengyi Z <shengyi.zhong@mail.mcgill.ca>
2024-11-21 19:04:35 -0500

    WIP

--- src/main/java/ca/mcgill/ecse/coolsupplies/javafx/fxml/controllers/StartOrderWindowController.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/javafx/fxml/controllers/StartOrderWindowController.java
@@ -1,9 +1,6 @@
 package ca.mcgill.ecse.coolsupplies.javafx.fxml.controllers;
 
-import ca.mcgill.ecse.coolsupplies.controller.CoolSuppliesFeatureSet1Controller;
-import ca.mcgill.ecse.coolsupplies.controller.CoolSuppliesFeatureSet2Controller;
-import ca.mcgill.ecse.coolsupplies.controller.TOParent;
-import ca.mcgill.ecse.coolsupplies.controller.TOStudent;
+import ca.mcgill.ecse.coolsupplies.controller.*;
 import ca.mcgill.ecse.coolsupplies.model.BundleItem;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,37 +10,59 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class StartOrderWindowController implements Initializable {
+
     @FXML
-    private Label parentLabel;
+    private ChoiceBox<String> parentChoiceBox;
     @FXML
-    private ChoiceBox<TOStudent> studentChoiceBox;
+    private ChoiceBox<String> studentChoiceBox;
     @FXML
     private ChoiceBox<BundleItem.PurchaseLevel> levelChoiceBox;
     @FXML
     private TextField idTextField;
 
     Integer id;
-    List<TOStudent> students = CoolSuppliesFeatureSet2Controller.getStudents();
+    TOStudent selectedStudent;
+    TOParent selectedParent;
+    List<TOParent> parents = CoolSuppliesFeatureSet1Controller.getParents();
+    List<TOStudent> students;
     List<BundleItem.PurchaseLevel> levels = Arrays.asList(BundleItem.PurchaseLevel.values());
 
     void selectID(ActionEvent event) {
 
     }
 
+
+
     @FXML
     void goToAccount() {
 
     }
 
+    void getParent(ActionEvent event) {
+        selectedParent = CoolSuppliesFeatureSet1Controller.getParent(parentChoiceBox.getValue());
+        if (selectedParent != null) {
+            students = CoolSuppliesFeatureSet6Controller.getStudentsOfParent(selectedParent.getEmail());
+            List<String> studentNames = new ArrayList<>();
+            for (TOStudent student : students) {
+                studentNames.add(student.getName());
+            }
+            studentChoiceBox.getItems().addAll(studentNames);
+        }
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        parentLabel.setText("");
-        studentChoiceBox.getItems().addAll(students);
+        List<String> parentNames = new ArrayList<>();
+        for (TOParent parent : parents) {
+            parentNames.add(parent.getEmail());
+        }
+        parentChoiceBox.getItems().addAll(parentNames);
         levelChoiceBox.getItems().addAll(levels);
     }
 }
