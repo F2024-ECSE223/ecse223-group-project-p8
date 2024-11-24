b471b59cbd2203d63ad3c506588dc39c65db896b
Shengyi Z <shengyi.zhong@mail.mcgill.ca>
2024-11-23 22:15:10 -0500

    update UpdateOrderWindow & Controller

--- src/main/java/ca/mcgill/ecse/coolsupplies/javafx/fxml/controllers/UpdateOrderWindowController.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/javafx/fxml/controllers/UpdateOrderWindowController.java
@@ -3,9 +3,15 @@ package ca.mcgill.ecse.coolsupplies.javafx.fxml.controllers;
 import ca.mcgill.ecse.coolsupplies.controller.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.*;
 
@@ -18,6 +24,9 @@ public class UpdateOrderWindowController implements Initializable {
 
     @FXML
     private ChoiceBox<String> levelChoiceBox;
+
+    @FXML
+    private Button BackButton;
 
     private TOOrder order;
     String studentName;
@@ -80,5 +89,17 @@ public class UpdateOrderWindowController implements Initializable {
             getStudent(event);
             getLevel(event);
         });
+    }
+
+    private void loadPage(String fxmlPath) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+        AnchorPane newPage = loader.load();
+        Stage currentStage = (Stage) BackButton.getScene().getWindow();
+        currentStage.setScene(new Scene(newPage));
+    }
+
+    @FXML
+    private void goBack() throws IOException {
+        loadPage("/pages/ViewOrderWindow.fxml");
     }
 }
