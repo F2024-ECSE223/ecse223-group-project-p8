5b6bec6b37fa1d6e347f0352b5c57204cd884fa9
sen-snigdha <snigdha.dawson@gmail.com>
2024-11-24 10:30:59 -0500

    Still debugging

--- src/main/java/ca/mcgill/ecse/coolsupplies/javafx/fxml/controllers/UpdateOrderItemQuantityDialogController.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/javafx/fxml/controllers/UpdateOrderItemQuantityDialogController.java
@@ -37,6 +37,7 @@ public class UpdateOrderItemQuantityDialogController {
     }
 
     public void setQty(Integer itemQty){
+        qtySpinner.getValueFactory().setValue(itemQty);
     }
     //--------------------------------------------------------------------------------
 
